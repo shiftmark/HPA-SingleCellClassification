9b97e335d0768390186be14d4f44eb7aa2121304
Adrian Bratu <adrian.bratu@live.com>
2021-05-16 00:47:17 +0300

    a

--- tests/test-create_tfrecord.py
+++
@@ -1,4 +0,0 @@
-from utils.create_tfrecord import CreateTFRecord
-
-label_dict = {'a1':1, 'a2':2, 'b1':3, 'c1':4, 'c2':5, 'c3':6, 'c4':7}
-CreateTFRecord(img_dir='/tests/images/', label_dict=label_dict, save_to='/tests/tfrecords/')
