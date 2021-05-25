dbfcaf57940caed3326bd198d40cf0e20de27d55
Adrian <adrian.bratu@live.com>
2021-05-26 00:26:58 +0300

    Drop 'Image'

--- src/train/pipeline.py
+++ src/train/pipeline.py
@@ -2,8 +2,10 @@ import sys
 import os
 import random
 import cv2
+import glob
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import pandas as pd
 
 sys.path.insert(0, './src/')
 
@@ -17,7 +19,7 @@ import train.constants as c
 
 def download(num_img=c.NUM_IMGS, to_dir=c.SAVE_IMG_TO):
     to_download = c.DF_CL[:num_img]
-    print(f'Attempting to download {len(to_download)} x4 images. One for each RGBY channel.')
+    print(f'Attempting to download {len(to_download)} x 4 images. One for each RGBY channel.')
 
     for idx, row in to_download.iterrows():
         try:
@@ -36,7 +38,7 @@ def download(num_img=c.NUM_IMGS, to_dir=c.SAVE_IMG_TO):
                 color_channels.append(cv2.imread(os.path.join(save_path, file_name+c.IMG_FORMAT),
                                       cv2.IMREAD_GRAYSCALE))
             
-            if not os.path.exists(c.MULTICHANNEL_IMGS:
+            if not os.path.exists(c.MULTICHANNEL_IMGS):
                 os.makedirs(c.MULTICHANNEL_IMGS)
 
             cv2.imwrite(os.path.join(c.MULTICHANNEL_IMGS,
@@ -45,26 +47,32 @@ def download(num_img=c.NUM_IMGS, to_dir=c.SAVE_IMG_TO):
             print(f'Multichannel image {os.path.basename(img)+c.IMG_FORMAT} saved.')
         except:
             print(f'Failed to save multichannel image {os.path.basename(img)+c.IMG_FORMAT}')
-            
+
+def labels_df(df):
+    one_hot = pd.Series(df.Label_idx).str.get_dummies()
+    df = df.merge(one_hot, right_index=True, left_on=df.index)
+    df.drop([
+        'key_0',
+        'Label',
+        'Cellline',
+        'in_trainset',
+        'Label_idx'
+        ], inplace=True, axis=1) # Keep just 'Image', '0', '1', ..., '18' columns
+    
+    df['Name'] = df.Image.map(lambda x: x.split('/')[-1])
+    df['Labels'] = df[[str(s) for s in range(19)]].apply(lambda x: list(x), axis=1)
+    
+    df.drop([str(s) for s in range(19)], axis=1, inplace=True) # Drop '0', '1', ..., '18'
+    df.drop(['Image'], axis=1, inplace=True) # Drop 'Image'
+    df.set_index('Name', inplace=True)
+
+    return df
 
 # download and create tfrecord
-#download(100)
+download(9)
 
-df = c.DF
-# Create a pd.Series with one-hot encoded values and merge it with dataframe
-df =df.merge(pd.Series(c.DF.Label_idx).str.get_dummies(), right_index=True, left_on=df.index)
-# Keep just 'Image', '0', '1', ..., '18' columns
-df.drop(['key_0', 'Label', 'Cellline', 'in_trainset', 'Label_idx'], inplace=True, axis=1)
-# Create column 'Name' for image names
-df['Name'] = df.Image.map(lambda x: x.split('/')[-1])
-# Create 'Labels' column, containing one-hot encoded arrays
-df['Labels'] = df[[str(s) for s in range(19)]].apply(lambda x: list(x), axis=1)
-# Drop '0', '1', ..., '18' columns
-df.drop([str(s) for s in range(19)], axis=1, inplace=True)
-# Set 'Name' as index
-df.set_index('Name', inplace=True)
+CreateTFRecord(c.MULTICHANNEL_IMGS, labels_df(c.DF)).write_to(c.SAVE_TFREC_TO, num_items_in_record=13)
 
-CreateTFRecord(c.MULTICHANNEL_IMGS, df).write_to(c.SAVE_TFREC_TO, num_items_in_record=13)
 
 #segment
 
